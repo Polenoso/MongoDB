0a146be626c5a57b0b4c50593c4edbf746d6b5d1
aitorpagan <aitorpagan@mbp-de-aitor>
2015-12-02 20:24:54 +0100

    ETL terminado

--- src/Principal/ETL/Mongo.java
+++ src/Principal/ETL/Mongo.java
@@ -5,27 +5,29 @@
  */
 package Principal.ETL;
 
+import Modelo.Imagen;
+import Modelo.Metadata;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 
 import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
 import org.bson.Document;
 
 
 public class Mongo {
 
-   public static void main( String args[] ) {
+   public static void insertimage( Imagen img) {
        Document bson = null;
 	
       try{   
 		
          // To connect to mongodb server
          MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-			
+         bson = img.toBson();
          // Now connect to your databases
-         //DB db = mongoClient.getDB( "test" );
          MongoDatabase db = mongoClient.getDatabase("test");
          System.out.println("Connect to database successfully");
          
@@ -33,19 +35,16 @@ public class Mongo {
          System.out.println("Collection mycol selected successfully");
 			
         
-         bson = BsonMaker.getBSONobject();
          Document i = new Document().append("_id", bson.get("_id"));
-         if(coll.find(i)){
+         if(!coll.find(i).iterator().hasNext()){
              coll.insertOne(bson);
          }else{
              coll.findOneAndReplace(i, bson);
          }
          
-	 FindIterable cursor = coll.find();
-         MongoCursor x = cursor.iterator();		
-         while (x.hasNext()) { 
-            System.out.println(x.next());
-         }
+//	 FindIterable cursor = coll.find();
+//         MongoCursor x = cursor.iterator();		
+
 			
       }catch(Exception e){
          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
